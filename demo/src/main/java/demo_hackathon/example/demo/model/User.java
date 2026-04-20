a1f386533d2c7ba3bfce890edc123d15044da491
Eshwar954 <eshwarsaikandimalla@gmail.com>
2026-04-20 15:26:03 +0530

    fixed users entity

--- demo/src/main/java/demo_hackathon/example/demo/model/User.java
+++ demo/src/main/java/demo_hackathon/example/demo/model/User.java
@@ -9,10 +9,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name="users")
 public class User {
 
     @Id
